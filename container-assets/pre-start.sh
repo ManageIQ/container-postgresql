64196a0b0883f5dd52bdf80f4aa7db5e6a0d6f8e
Brandon Dunne <brandondunne@hotmail.com>
2022-12-08 18:25:20 -0500

    Put certificates in /etc/pki instead
    The problem with /var/lib/pgsql/data/userdata is if this is a new install,
    initalizing the database will fail because the userdata directory is not
    empty and it is expected to be empty.  Since the postgres configs are always
    mounted on the pod we need the certs to be in a predictable location.

--- container-assets/pre-start.sh
+++ container-assets/pre-start.sh
@@ -3,9 +3,9 @@
 if [ ! -f /opt/app-root/src/certificates/server.key ]; then
   echo "Skipping SSL setup, key not found."
 else
-  cp /opt/app-root/src/certificates/server.crt /var/lib/pgsql/data/userdata/server.crt
-  cp /opt/app-root/src/certificates/server.key /var/lib/pgsql/data/userdata/server.key
+  cp /opt/app-root/src/certificates/server.crt /etc/pki/tls/certs/server.crt
+  cp /opt/app-root/src/certificates/server.key /etc/pki/tls/private/server.key
 
   # Postgresql server will reject key files with liberal permissions
-  chmod og-rwx /var/lib/pgsql/data/userdata/server.key
+  chmod og-rwx /etc/pki/tls/private/server.key
 fi
