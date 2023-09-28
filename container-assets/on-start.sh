6d7f38d94d9f43ec51554857e8b9da7c790d4a61
Brandon Dunne <brandondunne@hotmail.com>
2023-09-28 16:45:41 -0400

    Conditionally inject the postgres SSL config after other configs
    This needs to happen after generate_postgresql_config creates the ephemeral
    config file which includes all configs from mounted volumes so that these
    ssl settings are loaded last overwriting anything in included configs.
    It also needs to happen before try_pgupgrade could potentially try to start
    a server and before pg_ctl tries to start the server.
    We also need to ensure hostssl in pg_hba.conf after initialize_database in
    the case of a new empty volume.

--- container-assets/on-start.sh
+++ container-assets/on-start.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
 psql --command "ALTER ROLE \"${POSTGRESQL_USER}\" SUPERUSER;"
-
-if [ -f /etc/pki/tls/private/server.key ]; then
-  sed -i 's/host\(\b.*\)/hostssl\1/g' /var/lib/pgsql/data/userdata/pg_hba.conf
-
-  sed -i 's/.*ssl = off.*/ssl = on/g' /var/lib/pgsql/data/userdata/postgresql.conf
-  sed -i 's/.*ssl_cert_file.*/ssl_cert_file = \/etc\/pki\/tls\/certs\/server.crt/g' /var/lib/pgsql/data/userdata/postgresql.conf
-  sed -i 's/.*ssl_key_file.*/ssl_key_file = \/etc\/pki\/tls\/private\/server.key/g' /var/lib/pgsql/data/userdata/postgresql.conf
-fi
--- container-assets/pre-start.sh
+++ container-assets/pre-start.sh
@@ -3,6 +3,7 @@
 if [ ! -f /opt/app-root/src/certificates/server.key ]; then
   echo "Skipping SSL setup, key not found."
 else
+  echo "Copying SSL certificates to /etc/pki/ and fixing permissions"
   cp /opt/app-root/src/certificates/server.crt /etc/pki/tls/certs/server.crt
   cp /opt/app-root/src/certificates/server.key /etc/pki/tls/private/server.key
 
